6b7751372d8f9f16e92915d1128a5e9ee8e85857
dmach4 <dmachfour@gmail.com>
2023-06-30 17:40:55 -0300

    foto

--- json.js
+++ json.js
@@ -1,44 +1,29 @@
 let lafJson = [
-  {
-    id: 1,
+  { id: 1,
     name: 'Regulador.',
-    img: 'images/01.png',
+    img: 'images/02.png',
     price: [200.00, 400.00,300.00,555.00,666.00],
-    sizes: [
-      'GLP',
-      'ARGONIO',
-      'OXIGENIO',
-      'ACETILENO',
-      'DANIEL'
-      ],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
-  },
-  {
-    id: 2,
+    sizes: ['GLP','ARGONIO','OXIGENIO','ACETILENO','DANIEL'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'},
+
+  { id: 2,
     name: 'Maçarico.',
     img: 'images/02.png',
     price: [123.00, 123.00],
-    sizes: [
-      'LA2201',
-      'LA2200',
-      ' ',
-      ' ',
-      ' '
-    ],
-    description: 'Maçarico de Solda Modelo Dourado LA200'
-  },
-  {
-    id: 3,
+    sizes: ['LA2201','LA2200',' ',' ',' '],
+    description: 'Maçarico de Solda Modelo Dourado LA200'},
+
+  { id: 3,
     name: 'Regulador.',
-    img: 'images/01.png',
+    img: 'images/02.png',
     price: [300.00, 111.00,123.00],
-    sizes: [
-      'Primeiro',
-      'Segundo',
-      'Terceiro',
-      ' ',
-      ' '
-    ],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
-  }
+    sizes: ['Primeiro','Segundo','Terceiro',' ',' '],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'}
+
+
+
+
+
+
+
 ];
