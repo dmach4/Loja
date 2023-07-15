df1ceea7fd6bad3f35ecb6bb18aa2c1b0a0d28d2
dmach4 <dmachfour@gmail.com>
2023-07-15 20:37:44 -0300

    json

--- json.js
+++ json.js
@@ -1,37 +1,26 @@
 let lafJson = [
   { id: 1,
-    name: 'Regulador 200.',
-    img: 'images/02.png',
-    price: ['200.00'],
-    sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio <br> Corpo em Latão Usinado,Sistema de Pistão <br> NCM:00.000-00'},
-
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
   { id: 2,
-    name: 'Regulador 200.',
-    img: 'images/02.png',
-    price: ['200.00'],
-    sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio <br> Corpo em Latão Usinado,Sistema de Pistão <br> NCM:00.000-00'},
-
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
   { id: 3,
-    name: 'Regulador 200.',
-    img: 'images/02.png',
-    price: ['200.00'],
-    sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio <br> Corpo em Latão Usinado,Sistema de Pistão <br> NCM:00.000-00'},
-
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
   { id: 4,
-    name: 'Regulador 200.',
-    img: 'images/02.png',
-    price: ['200.00'],
-    sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio <br> Corpo em Latão Usinado,Sistema de Pistão <br> NCM:00.000-00'},
-
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
   { id: 5,
-    name: 'Regulador 200.',
-    img: 'images/02.png',
-    price: ['200.00'],
-    sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio <br> Corpo em Latão Usinado,Sistema de Pistão <br> NCM:00.000-00'},
-
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
+  { id: 6,
+    name: 'Regulador 200.', img: 'imagens/02.png', price: ['200.00'], sizes: ['LA2200'],
+    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+  },
 ];
