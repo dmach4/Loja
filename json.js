4a8d684865d08e5ca073a6121f2fec46e69d0de5
dmach4 <dmachfour@gmail.com>
2023-07-20 09:36:57 -0300

    preencher lista

--- json.js
+++ json.js
@@ -1,18 +1,30 @@
 let lafJson = [
   { id: 1,
-    name: 'Maçarico 201.', img: 'imagens/ma201.png', price: ['2000.00'], sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+    name: 'Maçarico 200.', img: 'imagens/macarico200.png', price: ['123.50'], sizes: ['LA2200'],
+    description: 'Extesões: 2, 4, 6, 9 e 12 para GLP / ACET. <br> Entrada: Oxigênio(dir.) e Gás(esq.), rosca 9/16"UNF. <br> Punho anatômico e resistente. <br> Registro de ponta cônica de inox. <br> Peso: 370g/0,37Kg. <br> NCM: 000.0000.'
   },
   { id: 2,
-    name: 'Maçarico 200.', img: 'imagens/macarico200.png', price: ['2000.00'], sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+    name: 'Maçarico 200 Fusão.', img: 'imagens/mac200f.png', price: ['430.00'], sizes: ['LA2200F'],
+    description: 'Extensão: 2, 4, 6 e bico de fusão. <br> Entrada: Oxigênio(dir.) e Gás(esq.), rosca 9/16"UNF. <br> Punho anatômico e resistente. <br> Registro de ponta cônica de inox. <br> Peso: 00g/0,00Kg. <br> NCM: 000.0000.'
   },
   { id: 3,
-    name: 'Regulador 200.', img: 'imagens/mac200f.png', price: ['2000.00'], sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
+    name: 'Regulador 201.', img: 'imagens/ma201.png', price: ['114.40'], sizes: ['LA2201'],
+    description: 'Extesões: 2, 4, 6, 9 e 12 para GLP / ACET. <br> Entrada: Oxigênio(dir.) e Gás(esq.), rosca 9/16"UNF. <br> Punho em tubo estriado em latão. <br> Melhor vazão para adaptar cabeça de corte. <br> Registro de ponta cônica de inox. <br> Peso: 570g/0,57Kg. <br> NCM: 000.0000.'
   },
-  { id: 4,
-    name: 'Regulador 200.', img: 'imagens/macarico200p.png', price: ['2000.00'], sizes: ['LA2200'],
-    description: 'Regulador de Pressão para Oxigênio, Corpo em Latão Usinado, Sistema de Pistão'
-  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
